d584d05a0c7a12220a2aa4e8509c42888cc35cd7
fragargon <fragargon@gmail.com>
2018-05-09 21:10:57 +0200

    add class bookDbHelper and contract class

--- app/src/main/java/com/example/android/inventoryapp/data/BookContract.java
+++ app/src/main/java/com/example/android/inventoryapp/data/BookContract.java
@@ -1,4 +1,23 @@
 package com.example.android.inventoryapp.data;
 
-public class BookContract {
+import android.provider.BaseColumns;
+
+public final class BookContract {
+
+    /** Create a private constructor because no one should ever create a {@link BookContract} object. */
+    private BookContract() {}
+
+    public static final class BookEntry implements BaseColumns {
+
+        // Table name
+        public final static String TABLE_NAME = "books";
+
+        // Column name
+        public final static String COLUMN_KEY = BaseColumns._ID;
+        public final static String COLUMN_PRODUCT_NAME = "product";
+        public final static String COLUMN_PRICE = "price";
+        public final static String COLUMN_QUANTITY = "quantity";
+        public final static String COLUMN_SUPPLIER_NAME = "name";
+        public final static String COLUMN_SUPPLIER_PHONE = "phone";
+    }
 }
--- app/src/main/java/com/example/android/inventoryapp/data/bookDbHelper.java
+++ app/src/main/java/com/example/android/inventoryapp/data/bookDbHelper.java
@@ -1,4 +1,60 @@
 package com.example.android.inventoryapp.data;
 
-public class bookDbHelper {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import com.example.android.inventoryapp.data.BookContract.BookEntry;
+
+public class bookDbHelper extends SQLiteOpenHelper {
+
+    private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME = "catalog.db";
+    private static final String LOG_TAG = "Create SQL table: ";
+
+    /**
+     * @param context to use to open or create the database
+     */
+    public bookDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    /**
+     * Called when the database is created for the first time. This is where the
+     * creation of tables and the initial population of the tables should happen.
+     *
+     * @param db The database.
+     */
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+        final String SQL_CREATE_BOOKS_TABLE = "CREATE TABLE " + BookEntry.TABLE_NAME + " (" +
+                BookEntry.COLUMN_KEY + "INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                BookEntry.COLUMN_PRODUCT_NAME + "TEXT NOT NULL, " +
+                BookEntry.COLUMN_PRICE + "INTEGER NOT NULL, " +
+                BookEntry.COLUMN_QUANTITY + "INTEGER NOT NULL DEFAULT 0, " +
+                BookEntry.COLUMN_SUPPLIER_NAME + "TEXT NOT NULL, " +
+                BookEntry.COLUMN_SUPPLIER_PHONE + "TEXT NOT NULL" + ");";
+
+        db.execSQL(SQL_CREATE_BOOKS_TABLE);
+
+        // Log the table creation
+        Log.v(LOG_TAG, SQL_CREATE_BOOKS_TABLE);
+
+    }
+
+    /**
+     *
+     * @param db         The database.
+     * @param oldVersion The old database version.
+     * @param newVersion The new database version.
+     */
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+        db.execSQL("DROP TABLE IF EXISTS " + BookEntry.TABLE_NAME);
+        onCreate(db);
+
+    }
 }

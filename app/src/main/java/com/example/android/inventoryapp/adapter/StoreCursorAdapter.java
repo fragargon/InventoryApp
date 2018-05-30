65ce7114faa0e89ed098a58ef2442b9394577d9f
fragargon <fragargon@gmail.com>
2018-05-31 00:33:35 +0200

    add sold button logic

--- app/src/main/java/com/example/android/inventoryapp/adapter/StoreCursorAdapter.java
+++ app/src/main/java/com/example/android/inventoryapp/adapter/StoreCursorAdapter.java
@@ -1,20 +1,29 @@
 package com.example.android.inventoryapp.adapter;
 
 import android.app.LoaderManager;
+import android.content.ContentUris;
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.CursorLoader;
 import android.database.Cursor;
+import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CursorAdapter;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.android.inventoryapp.R;
 import com.example.android.inventoryapp.data.StoreContract.StoreEntry;
 
+import java.text.NumberFormat;
+
 public class StoreCursorAdapter extends CursorAdapter{
+
+    private static final String LOG_TAG = StoreCursorAdapter.class.getSimpleName();
+
     /**
      * Constructor that always enables auto-requery.
      *
@@ -51,40 +60,65 @@ public class StoreCursorAdapter extends CursorAdapter{
      * @param cursor  The cursor from which to get the data. The cursor is already
      */
     @Override
-    public void bindView(View view, Context context, Cursor cursor) {
+    public void bindView(View view, final Context context, Cursor cursor) {
         // Find fields to populate in inflated template.
         TextView tvProductTitle = view.findViewById(R.id.list_product_title);
         TextView tvProductName = view.findViewById(R.id.list_product_name);
         TextView tvProductPrice = view.findViewById(R.id.list_product_price);
         TextView tvProductQuantity = view.findViewById(R.id.list_product_quantity);
-        ImageButton ibDecrement = view.findViewById(R.id.list_btn);
+        ImageButton ibSoldItem = view.findViewById(R.id.list_btn);
 
         // Extract properties from cursor.
         int titleColumnIndex = cursor.getColumnIndex(StoreEntry.COLUMN_PRODUCT_TITLE);
         int nameColumnIndex = cursor.getColumnIndex(StoreEntry.COLUMN_PRODUCT_NAME);
         int priceColumnIndex = cursor.getColumnIndex(StoreEntry.COLUMN_PRICE);
         int quantityColumnIndex = cursor.getColumnIndex(StoreEntry.COLUMN_QUANTITY);
+        final int selectedProductId = cursor.getColumnIndex(StoreEntry.COLUMN_KEY);
 
         // Read the catalog attribute from the cursor for the current item.
         int titleValue = cursor.getInt(titleColumnIndex);
         // Convert the value from the database (from 0 to 7) to a string.
-        // for the product type
-        String[] title = context.getResources().getStringArray(R.array.product_type);
-        String name = cursor.getString(nameColumnIndex);
-        float price = cursor.getFloat(priceColumnIndex);
-        int quantity = cursor.getInt(quantityColumnIndex);
+        // for the product type..
+        final String[] title = context.getResources().getStringArray(R.array.product_type);
+        // Extract the data from the cursor
+        final String name = cursor.getString(nameColumnIndex);
+        final float price = cursor.getFloat(priceColumnIndex);
+        final int quantity = cursor.getInt(quantityColumnIndex);
+        final int productId = cursor.getInt(selectedProductId);
 
         // Populate fields with extracted properties.
-        tvProductName.setText(name);
+        // Set text for product title
         tvProductTitle.setText(title[titleValue]);
-        tvProductPrice.setText(String.valueOf(price));
+        // Set the text for the product name
+        tvProductName.setText(name);
+        // Set the price with the correct currency.
+        NumberFormat currentCurrency = NumberFormat.getCurrencyInstance();
+        tvProductPrice.setText(currentCurrency.format(price));
+        // Set the number for the quantity.
         tvProductQuantity.setText(String.valueOf(quantity));
 
         // Set the listener to decrement the stock.
-        ibDecrement.setOnClickListener(new View.OnClickListener() {
+        ibSoldItem.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // TODO
+                if(quantity > 0) {
+                    int newQuantity = quantity - 1;
+                    Uri quantityUri = ContentUris.withAppendedId(StoreEntry.CONTENT_URI, productId);
+                    ContentValues values = new ContentValues();
+                    values.put(StoreEntry.COLUMN_QUANTITY, newQuantity);
+                    int rowUpdated = context.getContentResolver().update(quantityUri,
+                            values, null, null);
+                    if(!(rowUpdated > 0)) {
+                        Toast.makeText(context, context.getString(R.string.product_sold_error),
+                                Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(context, context.getString(R.string.product_sold),
+                                Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(context, context.getString(R.string.product_sold_stock_zero),
+                            Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
